2ef1fdb66fc70f0dd15a783648843f858ef651fd
Alejo Conde <alejo.conde.97@gmail.com>
2024-01-05 20:42:56 -0300

    formulario: validación de formato de fecha, email y teléfono

--- js/verify.js
+++ js/verify.js
@@ -6,6 +6,28 @@ const vacio = (campo) => {
   return estaVacio
 }
 
+const verificar = (texto, exp) => {
+  if(texto.match(exp)) {
+    return true
+  } else {
+   return false 
+  }
+}
+
+const verificarFecha = (fecha) => {
+  const mEdad = 18
+    const hoy = new Date()
+    const fechaValida = new Date(hoy.getFullYear() - mEdad, hoy.getMonth(), hoy.getDate(), 0, 0, 0)
+
+    const fValor = new Date(fecha)
+    fValor.setHours(fValor.getHours() + fValor.getTimezoneOffset()/60)
+    if(fValor < fechaValida) {
+      return true
+    } else {
+      return false
+    }
+}
+
 const checkSubmission = () => {
   const n = document.getElementById("fNombre")
   const a = document.getElementById("fApellido")
@@ -33,16 +55,32 @@ const checkSubmission = () => {
     telInvalid.innerHTML = medioVacioMensaje
     emailInvalid.innerHTML = medioVacioMensaje
   } else {
-    tel.classList.remove("is-invalid")
-    e.classList.remove("is-invalid")
+    if(!vacio(tel.value) && !verificar(tel.value, /^(?:([+]\d{1,4})[-.\s]?)?(?:[(](\d{1,3})[)][-.\s]?)?(\d{1,4})[-.\s]?(\d{1,4})[-.\s]?(\d{1,9})$/)) {
+      tel.classList.add("is-invalid")
+      telInvalid.innerHTML = "El teléfono ingresado no es válido"
+    } else {    
+      tel.classList.remove("is-invalid")
+    }
+  
+    if(!vacio(e.value) && !verificar(e.value, ".+@.+\..+")) {
+      e.classList.add("is-invalid")
+      emailInvalid.innerHTML = "El email ingresado no es válido"
+    } else {    
+      e.classList.remove("is-invalid")
+    }
   }
 
   const dateInvalid = document.getElementById("invalid-fecha")
   if(vacio(f.value)) {
     f.classList.add("is-invalid")
     dateInvalid.innerHTML = "Por favor, ingrese una fecha de nacimiento"
-  } else {
-    f.classList.remove("is-invalid")
+  } else {    
+    if(!verificarFecha(f.value)) {
+      f.classList.add("is-invalid")
+      dateInvalid.innerHTML = "La fecha de nacimiento ingresada no es válida"
+    }else {
+      f.classList.remove("is-invalid")
+    }
   }
 
 }
